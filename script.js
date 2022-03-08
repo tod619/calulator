a830335f466abb7b0090ce3a61cd0477ccc8e845
tod619 <trevorodwyer619@hotmail.com>
2022-03-08 21:05:16 +0000

    add operator functionality in script.js file

--- script.js
+++ script.js
@@ -2,18 +2,48 @@ const calculatorDisplay = document.querySelector('h1')
 const inputBtns = document.querySelectorAll('button')
 const clearBtn = document.getElementById('clear-btn')
 
+let firstValue = 0
+let operatorValue = ''
+let awaitingNextValue = false 
+
 function sendNumbeValue(number) {
     
-    // if current display number is 0 replace if not add number
-    const displayValue = calculatorDisplay.textContent
-    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number 
+    // Replace currentDisplayValue if firstValue is entered
+    if(awaitingNextValue) {
+        calculatorDisplay.textContent = number
+        awaitingNextValue = false 
+    } else {
+        // if current display number is 0 replace if not add number
+        const displayValue = calculatorDisplay.textContent
+        calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number
+    }
 }
 
 // Add 1 decimal point in the number
 function addDecimal() {
+    // if operator is clicked don't add decimal after
+    if(awaitingNextValue) return 
+
     if(!calculatorDisplay.textContent.includes('.')) {
         calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
     }
+}
+
+// Use operator
+function useOperator(operator) {
+    const currentValue = Number(calculatorDisplay.textContent)
+    // Assign currentValue to firstValue if firstValue has no value
+    if(!firstValue) {
+        firstValue = currentValue
+    }else {}
+
+    // Ready for the next value
+    awaitingNextValue = true
+
+    
+    operatorValue = operator
+
+
 }
 
 // Add Eventlisnters for numbers, operatoros, & decimals buttons
@@ -21,15 +51,19 @@ inputBtns.forEach((inputBtn) => {
     if(inputBtn.classList.length === 0) {
         inputBtn.addEventListener('click', () => sendNumbeValue(inputBtn.value))
     } else if(inputBtn.classList.contains('operator')) {
-        inputBtn.addEventListener('click', () => sendNumbeValue(inputBtn.value))
+        inputBtn.addEventListener('click', () => useOperator(inputBtn.value))
     } else if(inputBtn.classList.contains('decimal')) {
         inputBtn.addEventListener('click', () => addDecimal())
     }
 })
 
-// Reset the value in the display
+// Reset the value in the display + all other values
 function resetAll() {
+    firstValue = 0
+    operatorValue = ''
+    awaitingNextValue = false
     calculatorDisplay.textContent = '0'
+    
 }
 
 clearBtn.addEventListener('click', resetAll)
